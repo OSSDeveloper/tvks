453298f1e63c92d63bbe6ad65e481b0926c9ec90
vijay <vijay@processbricks.com>
2024-05-22 12:34:38 +0530

    added pm2 config.js file

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps : [{
+      name      : 'tvks',
+      script    : './venv/bin/unicorn', // assuming Unicorn is installed in a virtual environment
+      args: ['main:app', '--workers', '1'],
+      env: {
+        "FAST_API_HOST": "0.0.0.0",
+        "FAST_API_PORT": "8000"
+      }
+    }]
+  }
