4b5c1aa5b21229c84ba3e0c9b5cf5718dfc787db
vijay <vijay@processbricks.com>
2024-05-22 12:35:58 +0530

    corrected unicorn to uvicorn

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
       name      : 'tvks',
-      script    : './venv/bin/unicorn', // assuming Unicorn is installed in a virtual environment
+      script    : './venv/bin/uvicorn', // assuming Unicorn is installed in a virtual environment
       args: ['main:app', '--workers', '1'],
       env: {
         "FAST_API_HOST": "0.0.0.0",
