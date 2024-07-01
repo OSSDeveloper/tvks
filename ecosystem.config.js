442e30a8a43c6620bddf492cd36cf185e693cc19
vijay <vijay@processbricks.com>
2024-07-01 08:53:50 +0530

    modified ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,11 @@
 module.exports = {
     apps : [{
       name      : 'tvks',
-      script    : '/opt/apps/tvks/.venv/bin/uvicorn', // assuming Unicorn is installed in a virtual environment
-      args: ["main:app", "--host", "0.0.0.0", "--port", "8000"],
+      script    : 'uvicorn', // assuming Unicorn is installed in a virtual environment
+      args: "main:app --host 0.0.0.0 --port 8000",
       env: {
         // NODE_ENV: "production"
-      }
-    }],
-    exec_mode: 'fork',
-    instances: 1
+      },
+      instances: 1
+    }]
   }
