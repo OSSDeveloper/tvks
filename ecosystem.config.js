dfc6e17fcc507f15b3929f8cae45103b22b0a8b6
vijay <vijay@processbricks.com>
2024-07-01 08:23:23 +0530

    updated ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,12 @@
 module.exports = {
     apps : [{
       name      : 'tvks',
-      script    : 'python', // assuming Unicorn is installed in a virtual environment
-      args: "main.py",
+      script    : 'uvicorn', // assuming Unicorn is installed in a virtual environment
+      args: "main:app --host 0.0.0.0 --port 8000",
       env: {
-        NODE_ENV: "production",
-        PORT: 8000
+        NODE_ENV: "production"
       }
-    }]
+    }],
+    exec_mode: 'fork',
+    instances: 1
   }
