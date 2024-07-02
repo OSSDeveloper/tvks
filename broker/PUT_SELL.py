45d1cd15bb25d19fb72d767b1aff5ba5c2a12a09
vijay <vijay@processbricks.com>
2024-07-02 12:35:14 +0530

    updated pnl display after sell

--- broker/PUT_SELL.py
+++ broker/PUT_SELL.py
@@ -55,15 +55,21 @@ async def sell_put(signal):
         order_report = order_report['data']
         if 'stat' not in order_report or order_report['stat'] != 'Ok':
             await close_positions(signal['option_type'])
+            positions = check_kotak_positions(kotak_positions)
+            print(f"Global PNL is : {settings.data['PNL']}")
             return True
             
         order_report = order_report['data']
         order_passed = not any(obj.get('ordSt') == 'rejected' for obj in order_report)
         if order_passed:
             recorded = await post_trans_tasks(signal,instrument, qty)
+            positions = check_kotak_positions(kotak_positions)
+            print(f"Global PNL is : {settings.data['PNL']}")
             return f"Order {signal['tag']} to SELL {instrument} : {qty} quantity IS PLACED."
         else:
             await close_positions(signal['option_type'])
+            positions = check_kotak_positions(kotak_positions)
+            print(f"Global PNL is : {settings.data['PNL']}")
             return True
             
     except Exception as e:
